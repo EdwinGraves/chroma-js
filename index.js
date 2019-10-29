d11cd753d854c3974d7d7a44781e1cc656f3e63e
Tomasz Kwolek <tomasz.kwolek@flightscopetennis.com.pl>
2019-10-29 22:34:48 +0100

    index.js added

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./lib/chroma-js/ChromaApp"));
+__export(require("./lib/chroma-js/ChromaInstance"));
+__export(require("./lib/chroma-js/AppInfo"));
+__export(require("./lib/chroma-js/Color"));
+__export(require("./lib/chroma-js/Key"));
+__export(require("./lib/chroma-js/Devices/Base"));
+__export(require("./lib/chroma-js/Animation"));
+__export(require("./lib/chroma-js/Devices/Keyboard"));
+//# sourceMappingURL=index.js.map
